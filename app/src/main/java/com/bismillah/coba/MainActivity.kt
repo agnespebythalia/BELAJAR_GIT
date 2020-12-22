08aa6c49c7fff3ece4fc3642eb60339ddee7f920
agnespebythalia <agnes.05@students.amikom.ac.id>
2020-12-22 22:17:17 +0700

    ketik hallo

--- app/src/main/java/com/bismillah/coba/MainActivity.kt
+++ app/src/main/java/com/bismillah/coba/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tes =""
+        val tes ="hallo"
     }
 }
